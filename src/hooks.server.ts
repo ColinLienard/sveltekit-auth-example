c09e026c667db70dc1164f140aa606906b2342cb
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-04-16 10:38:21 +0200

    Bump deps

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  secret: AUTH_SECRET,
+  trustHost: true
 })
